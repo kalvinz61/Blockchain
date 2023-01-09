d4fa4a9d7a46b0e43d3300104a4a6dbdcd5adb68
Kalvin <kalvinz61@gmail.com>
2023-01-09 16:00:26 -0500

    Starting blockchain

---
+++ blockchain.py
@@ -0,0 +1,29 @@
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+#Genisis Block
+#initialize chain (in it Function)
+#create block function
+#tools for chain
+
+class Blockchain :
+
+#Building a Blockchain
+  def __init__(self) -> None:
+    self.chain: list = []
+    self.create_block(proof = 1, previous_hash = '0')
+
+  def create_block(self, proof, previous_hash):
+    block = {'index': len(self.chain) +1,
+             'timestamp': str(datetime.datetime.now()),
+             'proof': proof,
+             'previous_has': previous_hash}
+    self.chain.append(block)
+    return block
+
+  def get_previous_block(self):
+    return self.chain[-1]
+
+#Mining our Blockchain
