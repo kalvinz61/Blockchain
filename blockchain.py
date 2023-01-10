a12ab51bedc0cc1007f8b8ee90d01ace3af7b6f3
Kalvin <kalvinz61@gmail.com>
2023-01-10 16:46:04 -0500

    added proof of work, hash block function

--- blockchain.py
+++ blockchain.py
@@ -26,4 +26,23 @@ class Blockchain :
   def get_previous_block(self):
     return self.chain[-1]
 
-#Mining our Blockchain
+  def proo_of_work(self, previous_proof):
+    new_proof = 1
+    check_proof = False
+
+    while check_proof is False:
+      hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+      if hash_operation[:4] == '0000' :
+        check_proof = True
+      else:
+        new_proof +=1
+    return new_proof
+
+  def hash(self, block):
+    encoded_block = json.dumps(block, sort_keys = True).encode()
+    return hashlib.sha256(encoded_block).hexdigest()
+
+
+
+
+        #Mining our Blockchain
